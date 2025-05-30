1a483796524aaa4228c1a1f5c551eaf343c23d1a
Lucas Menke <lucas_menke@icloud.com>
2025-05-30 18:15:52 +0200

    > cs_1

--- cs_1/km/src/main.cpp
+++ cs_1/km/src/main.cpp
@@ -22,13 +22,71 @@ void debug_print(PCSTR text) {
 }
 
 namespace driver {
-	namespace code {
-		// IOCTL codes
-	}
-}
+	// IOCTL codes
+		// input / output control > common way for drivers to communicate with user mode applications
+	namespace codes {
+		// used to setup the driver
+		constexpr ULONG attach =
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x696, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
 
-NTSTATUS DriverEntry() {
-	debug_print("[+] DriverEntry triggered");
+		// read process memory
+		constexpr ULONG read =
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x697, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+
+		// write process memory
+		constexpr ULONG write =
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x698, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+	} // namespace codes
+
+	// shares between user mode & kernel mode
+	struct Request {
+		HANDLE process_id;
+		PVOID target;
+		PVOID buffer;
+		SIZE_T size;
+		SIZE_T return_size;
+	};
+} // namespace driver
+
+
+// "teal" entry point
+NTSTATUS driver_main(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path) {
+	UNREFERENCED_PARAMETER(registry_path);
+
+	UNICODE_STRING device_name = {};
+	RtlInitUnicodeString(&device_name, L" \\Device\\CS_1");
+
+	// create driver device object
+	PDEVICE_OBJECT device_object = nullptr;
+	NTSTATUS status = IoCreateDevice(driver_object, 0, &device_name, FILE_DEVICE_UNKNOWN,
+		FILE_DEVICE_SECURE_OPEN, FALSE, &device_object);
+
+	if (status != STATUS_SUCCESS) {
+		debug_print("[-] failed to create driver device.\n");
+		return status;
+	}
+
+	debug_print("[+] driver device successfully created\n");
 
 	return STATUS_SUCCESS;
+}
+
+/*
+* normaly a ref to a driver object is passed to the entry point
+* because kdmapper is used they would be null
+*	kdmapper manually maps the driver so the os doesn't know about the driver & cannot provide a driver object
+*	the driver object needs to be manually created (springboard from DriverEntry() into a new entry point)
+*
+NTSTATUS DriverEntry(
+  _In_ PDRIVER_OBJECT  DriverObject,
+  _In_ PUNICODE_STRING RegistryPath
+);
+*/
+NTSTATUS DriverEntry() {
+	debug_print("[+] DriverEntry triggered\n");
+
+	UNICODE_STRING driver_name = {};
+	RtlInitUnicodeString(&driver_name, L"\\Driver\\CS_1"); // places the string (L"...") in the unicode_string structure
+
+	return IoCreateDriver(&driver_name, &driver_main);
 }
