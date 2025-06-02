17e5c0f42a4f7a665ad6626da5509bb24b8b2109
Lucas Menke <lucas_menke@icloud.com>
2025-06-02 07:32:56 +0200

    > um

--- cs_1/um/src/main.cpp
+++ cs_1/um/src/main.cpp
@@ -24,7 +24,7 @@ static DWORD get_process_id(const wchar_t* process_name) {
 			while (Process32NextW(snap_shot, &entry) == TRUE) {
 				if (_wcsicmp(process_name, entry.szExeFile) == 0) {
 					process_id = entry.th32ProcessID;
-					break; 
+					break;
 				}
 			}
 		}
@@ -68,10 +68,89 @@ static std::uintptr_t get_module_base(const DWORD pid, const wchar_t* module_nam
 	return module_base;
 }
 
+namespace driver {
+	// IOCTL codes
+		// input / output control > common way for drivers to communicate with user mode applications
+	namespace codes {
+		// used to setup the driver
+		constexpr ULONG attach =
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x696, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
 
+		// read process memory
+		constexpr ULONG read =
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x697, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+
+		// write process memory
+		constexpr ULONG write =
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x698, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+	} // namespace codes
+
+	// shares between user mode & kernel mode
+	struct Request {
+		HANDLE process_id;
+		PVOID target;
+		PVOID buffer;
+		SIZE_T size;
+		SIZE_T return_size;
+	};
+
+	bool attach_to_process(HANDLE driver_handle, const DWORD pid) {
+		Request r;
+		r.process_id = reinterpret_cast<HANDLE>(pid);
+
+		return DeviceIoControl(driver_handle, codes::attach, &r, sizeof(r), &r, sizeof(r), nullptr, nullptr);
+	}
+
+	template <class T>
+	T read_memory(HANDLE driver_handle, const std::uintptr_t addr) {
+		T temp = {};
+
+		Request r;
+		r.target = reinterpret_cast<PVOID>(addr);
+		r.buffer = &temp;
+		r.size = sizeof(T);
+
+		DeviceIoControl(driver_handle, codes::read, &r, sizeof(r), &r, sizeof(r), nullptr, nullptr);
+
+		return temp;
+	}
+
+	template <class T>
+	void write_memory(HANDLE driver_handle, const std::uintptr_t addr, const T& value) {
+		Request r;
+		r.target = reinterpret_cast<PVOID>(addr);
+		r.buffer = (PVOID)&value;
+		r.size = sizeof(T);
+
+		DeviceIoControl(driver_handle, codes::write, &r, sizeof(r), &r, sizeof(r), nullptr, nullptr);
+
+	}
+} // namespace driver 
 
 int main() {
-	std::cout << "Hello World\n" << std::endl;
+	// find pid of process
+	const DWORD pid = get_process_id(L"notepad.exe");
+	if (pid == 0) {
+		std::cout << "Failed to find notepad\n";
+		std::cin.get();
+		return 1;
+	}
+
+	// create a handle to the driver
+	const HANDLE driver = CreateFile(L"\\\\.\\CS_1", GENERIC_READ, 0, 
+		nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+	if (driver == INVALID_HANDLE_VALUE) {
+		std::cout << "Failed to create driver handle\n";
+		std::cin.get();
+		return 1;
+	}
+
+	if (driver::attach_to_process(driver, pid) == true) {
+		std::cout << "Attachment successfull\n";
+	}
+
+	CloseHandle(driver);
+	std::cin.get();
 
 	return 0;
 }
